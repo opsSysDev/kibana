7367b797cc3a408ee376b860a2c6323c7518ecc6
childe <heychilde@gmail.com>
2014-08-05 15:28:48 +0800

    remove ctrip

--- src/config.js
+++ src/config.js
@@ -29,8 +29,7 @@ function (Settings) {
      *  +elasticsearch: {server: "http://localhost:9200", withCredentials: true}+
      *
      */
-    //elasticsearch: "http://"+window.location.hostname+":9200",
-    elasticsearch: "http://10.8.84.90:443",
+    elasticsearch: "http://"+window.location.hostname+":9200",
 
     /** @scratch /configuration/config.js/5
      *
@@ -81,14 +80,12 @@ function (Settings) {
       'sparklines'
     ],
 
+    /*  user can save dashboard with a class and subclass;
+     *  with the entry panel, admin could setup a more clear&convenient entry **/
     dashboard_class: [
-        {'4xx-5xx':[]},
-        {'IIS':["公共服务","机票","火车票","途风", "营销&高端", "主题游", "团队游","技术管理&安全中心", "用户研究&设计中心", "地面业务", "攻略社区", "商业智能部", "商旅", "无线", "框架", "系统", "软件架构部",  "网站运营中心",  "国际业务", "搜索产品", "套餐","邮轮","用车","高端旅游"]},
-        {'MOBILE':["Trace","Client","Restful"]},
-        {'Cloud':[]},
-        {'OPS':[]},
-        {'IT':[]},
-        {'CC':[]}
+        {'Apache':["domain1","domain2"]},
+        {'IIS':["Public","Web Service"]},
+        {'Cloud':["Openstack","Vclient"]}
         ]
   });
 });
